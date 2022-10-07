900a4081c0dec1df0de9fd929af26418c4b6b7d9
Timo3D <imeatingsoap@gmail.com>
2022-10-08 01:25:18 +0800

    tye

--- examples.py
+++ examples.py
@@ -120,11 +120,10 @@ class UpdatersExample(Scene):
         )
         square = Square().to_edge(UP)
 
-        decimal.add_updater(lambda d: d.next_to(square, RIGHT))
+        decimal.add_updater(lambda d: d.move_to(square))
         decimal.add_updater(lambda d: d.set_value(square.get_center()[1]))
         self.add(square, decimal)
         self.play(
-            square.next_to(DOWN),
             square.animate.to_edge(DOWN),
             rate_func=there_and_back,
             run_time=5,
--- main.py
+++ main.py
@@ -1,23 +1,53 @@
+from venv import create
 from manim import *
 import numpy as np
 
 class GraphExample(Scene):
     def construct(self):
         ax = Axes((-3, 10), (-1, 8))
-        
         ax.add_coordinates()
-
-        curve = ax.plot(lambda x: 2 * np.sin(x))
-
-        # self.add(ax, curve)
+        curve = ax.plot(lambda x: -0.5*x**3 + 2*x**2 + 1)
+        area = ax.get_area(graph=curve, x_range=(0,2))
         self.play(
             Create(ax, run_time=3, lag_ratio=0.1),
-            Create(curve, run_time=3, lag_ratio=0.1)
+            Create(curve, run_time=3, lag_ratio=0.1),
         )
-
-        area = ax.get_area(graph=curve, x_range=(0,2))
 
         self.play(
-            DrawBorderThenFill(area)
+            Create(area, lag_ratio=0.5)
         )
+
+        self.wait(1)
+
+class RiemannSums(Scene):
+    def construct(self):
+        title = Tex("Riemann Sums")
+        title.move_to(UP * 3 + RIGHT * 4.5)
+
+        ax = Axes((-3, 10), (-1, 8))
+        ax.add_coordinates()
+        curve = ax.plot(lambda x: -0.5*x**3 + 2*x**2 + 2)
+
+        self.play(
+            Write(title),
+            Create(ax, run_time=3, lag_ratio=0.1),
+            Create(curve, run_time=3, lag_ratio=0.1),
+        )
+        
+        riemannSettings = {
+            "x_min": 0,
+            "x_max": 4,
+            "fill_opacity": 0.75,
+            "stroke_width": 0,
+        }
+
+        iterations = 6
+
+        self.rect_list = self.get_riemann_rectangles_list(curve, iterations, start_color = PURPLE, end_color = ORANGE, **riemannSettings)
+
+        flat_rects = self.get_riemann_rectangles(dx = 0.25, start_color = invert_color(PURPLE), end_color = invert_color(ORANGE), **riemannSettings)
+
+        rects = self.rect_list[0]
+
+        self.transform_between_riemann_rects(flat_rects, rects, replace_mobject_with_target_in_scene = True, run_time = 3)
         self.wait(1)
